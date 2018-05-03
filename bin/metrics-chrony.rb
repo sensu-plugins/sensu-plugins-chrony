f43ef75b8c92872d1166aa3a27c5b4be77baed2e
Jack Watroba <jack@build.com>
2018-05-03 00:16:37 -0700

    Update metrics-chrony.rb (#9)
    * Update metrics-chrony.rb
    
    Fixes "undefined method `empty?" error which was introduced in the latest version.
    
    * appease the cops
    
    * add a changelog on users behalf

--- bin/metrics-chrony.rb
+++ bin/metrics-chrony.rb
@@ -41,7 +41,7 @@ class ChronyMetrics < Sensu::Plugin::Metric::CLI::Graphite
       config[:scheme] = config[:host]
     end
 
-    chronystats = chronystats
+    chronystats = read_chronystats
     critical "Failed to get chronycstats from #{config[:host]}" if chronystats.empty?
     metrics = {
       config[:scheme] => chronystats
@@ -54,7 +54,7 @@ class ChronyMetrics < Sensu::Plugin::Metric::CLI::Graphite
     ok
   end
 
-  def chronystats
+  def read_chronystats
     num_val_pattern = /^[-+]?\d+(\.\d+)?\s/
 
     `chronyc tracking`.each_line.each_with_object({}) do |line, hash|
